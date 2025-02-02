61a20537d47108b66c1e9a3444c6ccdc9238d538
Mnojhegde <109811963+Mnojhegde@users.noreply.github.com>
2025-02-02 21:59:39 +0530

    Update UartTxMonitorBfm.sv

--- src/hdlTop/uartTxAgentBfm/UartTxMonitorBfm.sv
+++ src/hdlTop/uartTxAgentBfm/UartTxMonitorBfm.sv
@@ -167,9 +167,9 @@ interface UartTxMonitorBfm (input  bit   clk,
      
       @(posedge oversamplingClk);
       if(tx == 0)
-	`uvm_info(TxMonitor, $sformatf(" Stop bit is detected in Tx monitor "), UVM_LOW);
+	`uvm_info(TxMonitor, " Stop bit is detected in Tx monitor ", UVM_LOW);
       else
- 	`uvm_info(TxMonitor, $sformatf(" Stop bit is detected in Tx monitor "), UVM_LOW);
+ 	`uvm_info(TxMonitor, " Stop bit is detected in Tx monitor ", UVM_LOW);
     end
  endtask
 	
