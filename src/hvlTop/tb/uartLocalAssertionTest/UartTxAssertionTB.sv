794a37bd282ae0f241da864875ec2eceb29858fd
Mnojhegde <109811963+Mnojhegde@users.noreply.github.com>
2025-02-05 22:44:26 +0530

    Update UartTxAssertionTB.sv

--- src/hvlTop/tb/uartLocalAssertionTest/UartTxAssertionTB.sv
+++ src/hvlTop/tb/uartLocalAssertionTest/UartTxAssertionTB.sv
@@ -4,6 +4,9 @@
 `include "uvm_macros.svh"
 import uvm_pkg :: *;
 
+//-----------------------------------------------------------------------
+// tb file to test the assertions by giving locally generated stimulus
+//------------------------------------------------------------------------
 module UartTxAssertionTb;
   logic tx;
   bit clk = 0;
@@ -17,8 +20,12 @@ module UartTxAssertionTb;
   UartTxAssertions uartTxAssertions(.uartClk(clk),.uartTx(tx));
   
   initial begin
-      When_startBitisdetected_nextCycleDataTransferStarts_ThenOddParityIsCheckedFailCase_ThenStopBitDetected_AssertionFail();
-
+		When_startBitisdetected_nextCycleDataTransferStarts_ThenStopBitDetected_AssertionPass();
+		// When_startBitisdetected_nextCycleDataTransferStarts_ThenEvenParityIsCheckedPassCase_ThenStopBitDetected_AssertionPass();
+		// When_startBitisdetected_nextCycleDataTransferStarts_ThenEvenParityIsCheckedFailCase_ThenStopBitDetected_AssertionFail();
+		// When_startBitisdetected_nextCycleDataTransferStarts_ThenOddParityIsCheckedPassCase_ThenStopBitDetected_AssertionPass();
+		// When_startBitisdetected_nextCycleDataTransferStarts_ThenOddParityIsCheckedFailCase_ThenStopBitDetected_AssertionFail();
+		// When_startBitisFailedToDetect_AssertionFail();
   end 
 
   task  When_startBitisdetected_nextCycleDataTransferStarts_ThenStopBitDetected_AssertionPass();
