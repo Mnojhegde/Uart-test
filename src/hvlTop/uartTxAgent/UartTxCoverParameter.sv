da7aa6567790ffbdc1c125d10c87e463684e9edd
Mnojhegde <109811963+Mnojhegde@users.noreply.github.com>
2025-02-06 07:40:01 +0530

    Update UartTxCoverParameter.sv

--- src/hvlTop/uartTxAgent/UartTxCoverParameter.sv
+++ src/hvlTop/uartTxAgent/UartTxCoverParameter.sv
@@ -4,7 +4,7 @@
 package UartTxCoverParameter;
   parameter DATA_WIDTH = 5;
   parameter PARITY_ENABLED = 1;
- typedef enum bit  {EVEN_PARITY , ODD_PARITY}PARITY_TYPE;
+  typedef enum bit  {EVEN_PARITY , ODD_PARITY}PARITY_TYPE;
 endpackage : UartTxCoverParameter
 
 `endif
