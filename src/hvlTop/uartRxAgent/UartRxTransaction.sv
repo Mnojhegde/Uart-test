9f4890c33bd5a53da42b3048b16b3d135d1abc7b
Mnojhegde <109811963+Mnojhegde@users.noreply.github.com>
2025-02-06 07:56:45 +0530

    Update UartRxTransaction.sv

--- src/hvlTop/uartRxAgent/UartRxTransaction.sv
+++ src/hvlTop/uartRxAgent/UartRxTransaction.sv
@@ -1,17 +1,21 @@
 
 `ifndef UARTRXTRANSACTION_INLCLUDED_ 
 `define UARTRXTRANSACTION_INCLUDED_
+
 //--------------------------------------------------------------------------------------------
 // Class: UartRxTransaction
 // It's a transaction class that holds the UART data items for generating the stimulus
 //--------------------------------------------------------------------------------------------
 class UartRxTransaction extends uvm_sequence_item;
-  //register with factory so we can override with uvm method in future if necessary.
+  
   `uvm_object_utils(UartRxTransaction)
  
   //input signals
-  bit[DATA_WIDTH+5: 0]receivingData[];
-  bit [NO_OF_PACKETS-1:0]parity;
+  bit[DATA_WIDTH-1: 0]receivingData[];
+  bit [NO_OF_PACKETS-1:0] parity;
+	bit [NO_OF_PACKETS-1:0]parityError; 
+	bit [NO_OF_PACKETS-1:0]breakingError; 
+	bit [NO_OF_PACKETS-1:0]overrunError;
   
   //-------------------------------------------------------
   // Externally defined Tasks and Functions
@@ -25,9 +29,6 @@ endclass : UartRxTransaction
 //--------------------------------------------------------------------------------------------
 // Construct: new
 // Constructs the device1_tx object
-//  
-//
-// Parameters:
 // name - UartRxTransaction
 //--------------------------------------------------------------------------------------------
 function UartRxTransaction :: new(string name = "UartRxTransaction");
@@ -37,7 +38,6 @@ endfunction : new
 //--------------------------------------------------------------------------------------------
 // do_copy method
 //--------------------------------------------------------------------------------------------
-
 function void UartRxTransaction :: do_copy(uvm_object rhs);
   UartRxTransaction rhs1;
 
@@ -51,8 +51,7 @@ endfunction : do_copy
     
 //--------------------------------------------------------------------------------------------
 // do_compare method
-//-------------------------------------------------------------------------------------------- 
-    
+//--------------------------------------------------------------------------------------------   
 function bit UartRxTransaction :: do_compare(uvm_object rhs, uvm_comparer comparer = null);
   UartRxTransaction rhs1;
   if(! $cast(rhs1,rhs))
@@ -64,7 +63,6 @@ endfunction : do_compare
 // Function: do_print method
 // Print method can be added to display the data members values
 //--------------------------------------------------------------------------------------------
-    
 function void UartRxTransaction :: do_print(uvm_printer printer);
   super.do_print(printer);
   foreach(this.receivingData[i])
