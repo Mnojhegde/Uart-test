9c7f49271bdff509ed0de7e95917198bd2025481
Mnojhegde <109811963+Mnojhegde@users.noreply.github.com>
2025-02-06 07:18:41 +0530

    Update UartTxMonitorProxy.sv

--- src/hvlTop/uartTxAgent/UartTxMonitorProxy.sv
+++ src/hvlTop/uartTxAgent/UartTxMonitorProxy.sv
@@ -5,31 +5,34 @@
 // Class: device0_monitor_proxy
 //--------------------------------------------------------------------------------------------
 class UartTxMonitorProxy extends uvm_monitor;
-   //register with factory so can use create uvm_method and
-   //override in future if necessary
+	
   `uvm_component_utils(UartTxMonitorProxy)
- 
-  virtual UartTxMonitorBfm uartTxMonitorBfm;
-  UartTxPacketStruct uartTxPacketStruct;
-  UartTxAgentConfig uartTxAgentConfig;
-  UartTxTransaction uartTxTransaction;
-  //declaring analysis port for the monitor port
 
+	// virtual handle of transmitter monitor bfm
+  virtual UartTxMonitorBfm uartTxMonitorBfm;
+	
+	// handles for struct packet, transaction packet and config class
+  UartTxPacketStruct uartTxPacketStruct;
+	UartTxTransaction uartTxTransaction;
+  UartTxAgentConfig uartTxAgentConfig;
+
+	//event for controlling run phase drop objection
   event monitorSynchronizer;
+	
+	//declaring analysis port for the monitor port
   uvm_analysis_port#(UartTxTransaction) uartTxMonitorAnalysisPort;
   
-//-------------------------------------------------------
-// Externally defined Tasks and Functions
-//-------------------------------------------------------
+	//-------------------------------------------------------
+	// Externally defined Tasks and Functions
+	//-------------------------------------------------------
   
   extern function  new( string name = "UartTxMonitorProxy" , uvm_component parent = null);
   extern virtual function void build_phase( uvm_phase phase);
   extern virtual task run_phase(uvm_phase phase);
 endclass : UartTxMonitorProxy
+		
 //--------------------------------------------------------------------------------------------
 // Construct: new
-//
-// Parameters:
 // name - UartTxMonitorProxy
 // parent - parent under which this component is created
 //--------------------------------------------------------------------------------------------
@@ -45,7 +48,6 @@ endfunction : new
 //--------------------------------------------------------------------------------------------
 function void UartTxMonitorProxy :: build_phase( uvm_phase phase);
   super.build_phase(phase);
-
   if(!(uvm_config_db #(virtual UartTxMonitorBfm) :: get(this, "" , "uartTxMonitorBfm",uartTxMonitorBfm)))
    begin 
     `uvm_fatal(get_type_name(),$sformatf("FAILED TO GET VIRTUAL BFM HANDLE "))
@@ -54,7 +56,6 @@ function void UartTxMonitorProxy :: build_phase( uvm_phase phase);
    begin 
      `uvm_fatal(get_type_name(),$sformatf("FAILED TO GET AGENT CONFIG"))
    end 
-
    uartTxTransaction = UartTxTransaction :: type_id :: create("uartTxTransaction");
 endfunction : build_phase
     
@@ -64,50 +65,25 @@ endfunction : build_phase
 //  phase - uvm phase
 //--------------------------------------------------------------------------------------------
 task UartTxMonitorProxy :: run_phase(uvm_phase phase);
- UartConfigStruct uartConfigStruct;
- uartTxTransaction = UartTxTransaction::type_id::create("uartTxTransaction");
- UartTxConfigConverter :: from_Class(uartTxAgentConfig,uartConfigStruct);
-// phase.raise_objection(this);
- //uartTxMonitorBfm.WaitForReset();
-/* fork 
-  uartTxMonitorBfm.GenerateBaudClk(uartConfigStruct);
- join_none
- $display("****************emtering forever loop**************");
- forever begin
- uartTxMonitorBfm.StartMonitoring(uartTxPacketStruct , uartConfigStruct);
- $display("**********************I AM HERE****************");
-
-
- UartTxSeqItemConverter::toTxClass(uartTxPacketStruct , uartTxAgentConfig , uartTxTransaction);
-  foreach(uartTxTransaction.transmissionData[i])
-   $display("MONITOR HAS received %b",uartTxPacketStruct.transmissionData[i]);
-  $display("********parity result is %b*************",uartTxTransaction.parity);
-phase.drop_objection(this);
-//  ->monitorSynchronizer;
- end 
- */
-
- fork 
-  begin 
-   uartTxMonitorBfm.GenerateBaudClk(uartConfigStruct);
-  end 
-  begin 
+	UartConfigStruct uartConfigStruct;
+	UartTxConfigConverter :: from_Class(uartTxAgentConfig,uartConfigStruct);
+	//uartTxMonitorBfm.WaitForReset();
+	fork 
+		begin 
+			uartTxMonitorBfm.GenerateBaudClk(uartConfigStruct);
+		end 
+		begin 
       forever begin
-       UartTxTransaction uartTxTransaction_clone;
-       uartTxMonitorBfm.WaitForReset();
-       uartTxMonitorBfm.StartMonitoring(uartTxPacketStruct , uartConfigStruct);
-        $display("**********************I AM HERE****************");
-
-
-	UartTxSeqItemConverter::toTxClass(uartTxPacketStruct , uartTxAgentConfig , uartTxTransaction);
-	$cast(uartTxTransaction_clone, uartTxTransaction.clone());
-    	uartTxMonitorAnalysisPort.write(uartTxTransaction_clone);
-	$display("MONITOR HAS received %p",uartTxPacketStruct.transmissionData);
-	$display("********parity result is %p*************",uartTxTransaction.parity);
-	->monitorSynchronizer;
+				uartTxMonitorBfm.WaitForReset();
+				uartTxMonitorBfm.StartMonitoring(uartTxPacketStruct , uartConfigStruct);
+				UartTxSeqItemConverter::toTxClass(uartTxPacketStruct , uartTxAgentConfig , uartTxTransaction);
+				uartTxMonitorAnalysisPort.write(uartTxTransaction);
+				$display("MONITOR HAS received %p",uartTxPacketStruct.transmissionData);
+				$display("parity result is %b",uartTxTransaction.parity);
+				->monitorSynchronizer;
        end 
-  end 
- join_any
+		end 
+	join_any
 endtask : run_phase
 `endif
  
