da9c3528d1c477fcc58dfb90e87e4aba6245e824
Mnojhegde <109811963+Mnojhegde@users.noreply.github.com>
2025-02-06 07:58:44 +0530

    Update UartRxSeqItemConverter.sv

--- src/hvlTop/uartRxAgent/UartRxSeqItemConverter.sv
+++ src/hvlTop/uartRxAgent/UartRxSeqItemConverter.sv
@@ -12,16 +12,14 @@ class UartRxSeqItemConverter extends uvm_object;
   //-------------------------------------------------------
   // Externally defined Tasks and Functions
   //-------------------------------------------------------
-  
   extern function new( string name = "UartRxSeqItemConverter");
   extern static function void fromRxClass(input UartRxTransaction uartRxTransaction, input UartRxAgentConfig uartRxAgentConfig, output UartRxPacketStruct uartRxPacketStruct);
   extern static function void toRxClass(input UartRxPacketStruct uartRxPacketStruct, input UartRxAgentConfig uartRxAgentConfig, inout UartRxTransaction uartRxTransaction);
+    
 endclass :UartRxSeqItemConverter
     
 //--------------------------------------------------------------------------------------------
 // Construct: new
-//
-// Parameters:
 // name - UartRxSeqItemConverter
 //--------------------------------------------------------------------------------------------
 function UartRxSeqItemConverter :: new(string name = "UartRxSeqItemConverter");
@@ -31,13 +29,10 @@ endfunction : new
 //--------------------------------------------------------------------------------------------
 // Function: from_class
 // Converting seq_item transactions into struct data items
-//
-// Parameters:
 // name -UartRxTransaction, UartRxPacketStruct 
 //--------------------------------------------------------------------------------------------
 function void UartRxSeqItemConverter :: fromRxClass(input UartRxTransaction uartRxTransaction,input UartRxAgentConfig uartRxAgentConfig, output UartRxPacketStruct uartRxPacketStruct);
   int total_transmission = uartRxTransaction.receivingData.size();
-
   for(int receiving_number=0 ; receiving_number < total_transmission; receiving_number++)begin 
     for( int i=0 ; i< uartRxAgentConfig.uartDataType ; i++) begin  
       uartRxPacketStruct.receivingData[receiving_number][i] = uartRxTransaction.receivingData[receiving_number][i];
@@ -49,12 +44,9 @@ endfunction : fromRxClass
 //--------------------------------------------------------------------------------------------
 // Function: to_class
 //  Converting struct data items into seq_item transactions
-//
-// Parameters:
 //  name - UartRxPacketStruct,UartRxTransaction 
 //--------------------------------------------------------------------------------------------
-
-  function void UartRxSeqItemConverter :: toRxClass(input UartRxPacketStruct uartRxPacketStruct,input UartRxAgentConfig uartRxAgentConfig,inout UartRxTransaction uartRxTransaction);
+function void UartRxSeqItemConverter :: toRxClass(input UartRxPacketStruct uartRxPacketStruct,input UartRxAgentConfig uartRxAgentConfig,inout UartRxTransaction uartRxTransaction);
   int total_receiving = $size(uartRxPacketStruct.receivingData);
   uartRxTransaction.receivingData = new[total_receiving];
   for(int receiving_number=0 ; receiving_number < total_receiving; receiving_number++)begin 
@@ -63,5 +55,5 @@ endfunction : fromRxClass
     end
     uartRxTransaction.parity[receiving_number] = uartRxPacketStruct.parity[receiving_number];
   end
-  endfunction
+endfunction
 `endif
