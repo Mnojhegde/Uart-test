13cb40687cbd451feea7edfb47d63ae35d86b15c
Mnojhegde <109811963+Mnojhegde@users.noreply.github.com>
2025-02-06 12:25:10 +0530

    Update UartTxTransaction.sv

--- src/hvlTop/uartTxAgent/UartTxTransaction.sv
+++ src/hvlTop/uartTxAgent/UartTxTransaction.sv
@@ -8,11 +8,11 @@ class UartTxTransaction extends uvm_sequence_item;
 	`uvm_object_utils(UartTxTransaction)
    
    //input signals
-	rand logic [DATA_WIDTH-1 : 0] transmissionData[];
-  logic [NO_OF_PACKETS-1:0] parity;
-	logic [NO_OF_PACKETS-1:0]parityError; 
-	logic [NO_OF_PACKETS-1:0]breakingError; 
-	logic [NO_OF_PACKETS-1:0]overrunError;
+	rand bit [DATA_WIDTH-1 : 0] transmissionData[];
+  bit [NO_OF_PACKETS-1:0] parity;
+	bit [NO_OF_PACKETS-1:0]parityError; 
+	bit [NO_OF_PACKETS-1:0]breakingError; 
+	bit [NO_OF_PACKETS-1:0]overrunError;
    
 
    //-------------------------------------------------------
